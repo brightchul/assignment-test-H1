4fa94331bdc4f424cfa9d91c1e274e41516b1cae
brightchul <abhbtbb1@gmail.com>
2023-01-26 17:08:55 +0900

    feat: add check logic about window, item
    window, item에 대해서 체크 로직을 메서드화

--- src/stores/memoryLocalStorage.ts
+++ src/stores/memoryLocalStorage.ts
@@ -6,6 +6,7 @@ class MemoryLocalStorage {
     this.window = undefined;
     this.data = Object.create(null);
   }
+
   setWindow() {
     if (typeof window === 'undefined') {
       return;
@@ -13,32 +14,44 @@ class MemoryLocalStorage {
     this.window = window;
   }
   sync(key: string) {
-    if (this.window === undefined) {
+    if (!this.checkMemoryWindow()) {
       return;
     }
-    const jsonData = localStorage.getItem(key);
 
+    const jsonData = localStorage.getItem(key);
     if (typeof jsonData !== 'string') return;
     const data = JSON.parse(jsonData);
     Object.keys(data).forEach((key) => (this.data[key] = data[key]));
   }
 
+  checkMemoryWindow() {
+    return this.window !== undefined;
+  }
+
+  hasItem(key: string) {
+    if (!this.checkMemoryWindow()) {
+      return false;
+    }
+    const item = this.getItem(key);
+    return !!item;
+  }
+
   setItem(key: string, item: string) {
-    if (this.window !== undefined) {
+    if (this.checkMemoryWindow()) {
       localStorage.setItem(key, item);
     }
     this.data[key] = item;
   }
 
   getItem(key: string) {
-    if (this.window !== undefined) {
+    if (this.checkMemoryWindow()) {
       return localStorage.getItem(key);
     }
     return this.data[key] ?? null;
   }
 
   removeItem(key: string) {
-    if (this.window !== undefined) {
+    if (this.checkMemoryWindow()) {
       localStorage.removeItem(key);
     }
     delete this.data[key];
