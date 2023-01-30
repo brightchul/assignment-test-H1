c3b834f2efdef52a437d8745c47d773dd3ff2485
hoon <57323359+brightchul@users.noreply.github.com>
2023-01-31 00:29:19 +0900

    Fix to remove duplicate atom key warning (#3)
    * fix: add atomKey util function and apply

--- src/stores/atoms.ts
+++ src/stores/atoms.ts
@@ -2,12 +2,13 @@ import { atom } from 'recoil';
 
 import memoryLocalStorage from './memoryLocalStorage';
 import { Todo } from './types';
+import { atomKey } from './util';
 
 const initData: Todo[] = [];
 
 // NOTE(@brightchul): Recoil을 next.js 사용시 hot module replacement로 인해 동일 키 생성되는 문제 우회
 export const todoListState = atom<Todo[]>({
-  key: `todoListState/${Date.now()}`,
+  key: atomKey('todoListState'),
   default: initData,
   effects: [
     ({ setSelf, onSet }) => {
---
+++ src/stores/util.ts
@@ -0,0 +1,1 @@
+export const atomKey = (name: string) => `${name}-${Date.now()}`;
